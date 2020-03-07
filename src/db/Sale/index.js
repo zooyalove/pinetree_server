e4b9330ddb27c7909b8f1702aeffb8c2fcc7ad77
Park Kyungbong <dorry457@nate.com>
2020-03-07 18:27:19 +0900

    판매 모델 추가

---
+++ src/db/Sale/index.js
@@ -0,0 +1,59 @@
+const Mongoose = require("mongoose");
+const { MongoPrimary } = require("lib/util");
+
+const Schema = Mongoose.Schema;
+
+const Sale = new Schema({
+  _id: MongoPrimary,
+
+  // 매출처 아이디
+  store_id: {
+    type: String,
+    required: true
+  },
+
+  // 판매물품 리스트
+  sell_list: {
+    type: Array,
+    default: []
+  },
+
+  // 할인금액
+  discount: {
+    type: Number,
+    default: 0
+  },
+
+  // 부대비용
+  extra_charge: {
+    type: Number,
+    default: 0
+  },
+
+  // 매출금액 (sales or turnover)
+  turnover: {
+    type: Number,
+    default: 0
+  },
+
+  // 결제 (CASH: 현금, ACCOUNT: 통장입금, CARD: 카드)
+  payments: {
+    CASH: {
+      type: Number,
+      default: 0
+    },
+    ACCOUNT: {
+      type: Number,
+      default: 0
+    },
+    CARD: {
+      type: Number,
+      default: 0
+    }
+  },
+
+  // 판매일자
+  selled_at: Date
+});
+
+module.exports = Mongoose.model("sale", Sale);
