220004fc3b082e4dfa89d15fc28c3bae4b51423e
Park Kyungbong <dorry457@nate.com>
2020-03-10 20:55:19 +0900

    클래스 형식 바꿈

--- src/lib/dbNotify.ts
+++ src/lib/dbNotify.ts
@@ -1,30 +1,23 @@
 import { EventEmitter } from "events";
 
 // EventEmitter를 상속받은 몽고디비의 실시간 변화를 감지하기 위한 관찰자 및 알람역할 클래스
-// class DBNotify extends EventEmitter
-let _instance: DBNotify;
+// class DBNotify
+const _emitter = new EventEmitter();
 
-class DBNotify extends EventEmitter {
-  constructor() {
-    super();
-    if (_instance) {
-      return _instance;
-    }
-
-    _instance = this;
-  }
-
+class DBNotify {
   subscribe(channel: string, listener: any) {
-    this.on(channel, listener);
+    _emitter.on(channel, listener);
   }
 
   unsubscribe(channel: string, listener: any) {
-    this.removeListener(channel, listener);
+    _emitter.removeListener(channel, listener);
   }
 
   publish(channel: string, data: any) {
-    this.emit(channel, data);
+    _emitter.emit(channel, data);
   }
 }
 
-export default DBNotify;
+const dbNotify: DBNotify = new DBNotify();
+
+export default dbNotify;
