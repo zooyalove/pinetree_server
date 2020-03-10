96b00547bb65a0c46b14c62f01e29dc095f8b18d
Park Kyungbong <dorry457@nate.com>
2020-03-10 20:57:32 +0900

    테스트

--- src/api/v1/auth/auth.ctrl.ts
+++ src/api/v1/auth/auth.ctrl.ts
@@ -1,7 +1,12 @@
 import Member from "db/Member";
 import { IMiddleware } from "koa-router";
+import DBNotify from "lib/dbNotify";
 
-export const signIn: IMiddleware = ctx => {};
+export const signIn: IMiddleware = ctx => {
+  DBNotify.publish("signin", "안뇽~~~~~~!");
+  // ctx.status = 200;
+  // ctx.body = "하이~~";
+};
 
 export const signOut: IMiddleware = ctx => {};
 
@@ -10,10 +15,10 @@ export const register: IMiddleware = ctx => {};
 
 // 이메일로 부여된 코드 검증
 // Method: POST
-type RequestBody = {
-  verify_code: string;
-};
-
 export const verifyCode: IMiddleware = ctx => {
+  type RequestBody = {
+    verify_code: string;
+  };
+
   const { verify_code }: RequestBody = ctx.request.body;
 };
--- src/api/ws/index.ts
+++ src/api/ws/index.ts
@@ -5,9 +5,12 @@ import DBNotify from "lib/dbNotify";
 export default ws.all("/ws", ctx => {
   // ctx.websocket.id = nanoid();
 
-  ctx.websocket.on("message", message => {
-    console.log(message);
-    console.log(ctx.websocket);
+  DBNotify.subscribe("signin", (data: any) => {
+    ctx.websocket.send(data);
   });
+  // ctx.websocket.on("message", message => {
+  //   console.log(message);
+  //   console.log(ctx.websocket);
+  // });
   ctx.websocket.on("close", () => {});
 });
