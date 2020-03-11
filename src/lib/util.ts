af6d9324009914fb6ca5c8e34bfc1975f746f629
Park Kyungbong <dorry457@nate.com>
2020-03-11 21:25:51 +0900

    tokenizer method create

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,8 +1,21 @@
 import nanoid from "nanoid";
+import { ParameterizedContext } from "koa";
 
 export const MongoPrimary = {
   type: String,
   default: () => nanoid(10),
   unique: true,
   index: true
+};
+
+export const setCookieToken = (ctx: ParameterizedContext, token: string) => {
+  ctx.cookies.set("token", token, {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  });
+};
+
+export const generateToken = (): string => {
+  const token = nanoid();
+  return token;
 };
