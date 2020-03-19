7d7023a782babb2cbfc98db9b9acd213d1723ea7
Park Kyungbong <dorry457@nate.com>
2020-03-19 19:54:12 +0900

    업로드 클래스 모듈 일부 수정

--- src/lib/uploadFile.ts
+++ src/lib/uploadFile.ts
@@ -1,1 +1,33 @@
-export default function uploadFile() {}
+import { File } from "formidable";
+import crypto from "crypto";
+
+interface IUploadImageOptions {
+  /**
+   * {String} Sets the directory for placing file uploads in, default './upload'
+   */
+  destination?: string;
+
+  /**
+   * {String} Set the prefix name of filename,
+   */
+  prefix?: string;
+
+  /**
+   * {Boolean} Keep file name and extension
+   */
+  keepFilename?: boolean;
+}
+
+class UploadImage {
+  constructor(file: File, options?: IUploadImageOptions) {}
+
+  resize() {
+    return this;
+  }
+
+  createThumbnail() {}
+
+  save() {}
+}
+
+export default UploadImage;
