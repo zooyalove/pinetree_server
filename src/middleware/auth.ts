290ada63e7b9f17ae6b75fec0ec648ed0c3db933
Park Kyungbong <dorry457@nate.com>
2020-03-20 17:53:17 +0900

    middleware type 재설정

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,6 @@
-import { IMiddleware } from "koa-router";
+import { Middleware } from "koa";
 
-export const isAuth: IMiddleware = (ctx, next) => {
+export const isAuth: Middleware = (ctx, next) => {
   if (!ctx.url.includes("/api/v1/auth")) {
     if (!ctx.state.user_id) {
       return ctx.throw(401, "Member signin required");
