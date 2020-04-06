55c95fd2a7085b1a3e1e2fbb95df58c255433616
Zylovue <dorry457@nate.com>
2020-04-07 02:48:12 +0900

    image rotate and thumbnail generate

--- src/api/v1/file/file.ctrl.ts
+++ src/api/v1/file/file.ctrl.ts
@@ -5,6 +5,8 @@ import sharp from "sharp";
 import { uploadDir } from "../../../lib/dir";
 import { generateFilename } from "../../../lib/util";
 
+const prefix = "thumb";
+
 /**
  * 저장된 이미지 보기
  * GET /api/v1/file/:filename
@@ -54,20 +56,44 @@ export const uploadImage: IMiddleware = async ctx => {
     const metadata = await sharp(imagePath).metadata();
     console.log("Rotation : ", metadata.orientation);
 
-    const filename = `${generateFilename()}.jpg`;
+    const signCode = generateFilename();
+    const filename = `${signCode}.jpg`;
     const filePath = path.join(uploadDir, filename);
 
-    await sharp(imagePath)
-      .resize({ width: 500 })
-      .jpeg({
-        quality: 100
-      })
-      .toFile(filePath);
+    const image = await sharp(imagePath)
+      .jpeg({ quality: 100 })
+      .resize({ width: 500 });
+
+    // image rotation
+    if (metadata.orientation && metadata.orientation !== 1) {
+      switch (metadata.orientation) {
+        case 8: // 90 degree
+          image.rotate(90);
+          break;
+
+        case 6: // -90 degree
+          image.rotate(-90);
+          break;
+
+        case 3: // 180 degree
+          image.rotate(180);
+      }
+    }
+
+    const bufferImage = await image.toBuffer({ resolveWithObject: false });
+    const thumb = sharp(bufferImage)
+      .png()
+      .resize({ width: 100 });
+    const thumb_filename = `${prefix}_${filename}`.replace(".jpg", ".png");
+
+    image.toFile(filePath);
+    thumb.toFile(path.join(uploadDir, thumb_filename));
 
     fs.unlinkSync(imagePath);
 
     ctx.body = {
-      filename
+      image: filename,
+      thumbnail: thumb_filename
     };
   } catch (e) {
     console.log(e);
