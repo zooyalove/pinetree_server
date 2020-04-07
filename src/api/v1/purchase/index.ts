f2a2e63473f02f0c1498e107b7e4e11359bffd95
Park Kyungbong <dorry457@nate.com>
2020-04-07 18:06:46 +0900

    purchase 로직 수정시작

--- src/api/v1/purchase/index.ts
+++ src/api/v1/purchase/index.ts
@@ -3,4 +3,6 @@ import * as purchase from "./purchase.ctrl";
 
 const router = new Router();
 
+router.post("/", purchase.addPurchase);
+
 export default router;
--- src/api/v1/purchase/purchase.ctrl.ts
+++ src/api/v1/purchase/purchase.ctrl.ts
@@ -1,3 +1,7 @@
 import { Middleware } from "koa";
 
+/**
+ * 상품구입내용 등록
+ * POST /api/v1/purchase/
+ */
 export const addPurchase: Middleware = ctx => {};
